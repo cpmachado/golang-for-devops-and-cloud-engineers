948688bd1adb25a8fdb308092fe6e72da4f494de
cpmachado <cpmachado@protonmail.com>
2024-12-22 22:49:59 +0000

    Section 3 - Chapter 7
    Command Arguments With Conditionals

--- section3/main.go
+++ section3/main.go
@@ -7,7 +7,15 @@ import (
 
 func main() {
 	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Printf("Usage: %v <argument>\n", args[0])
+		os.Exit(1)
+	}
+
 	fmt.Println("hello, world")
 	fmt.Printf("Arguments: %v\n", args)
 	fmt.Printf("Arguments without program: %v\n", args[1:])
+
+	fmt.Printf("1st Argument: %v\n", args[1])
 }
