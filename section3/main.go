549881fb3c73a196abaa47ef5b48f59a8fc05611
cpmachado <cpmachado@protonmail.com>
2024-12-22 23:15:17 +0000

    Section 3 - Chapter 10
    Parse HTTP JSON response

--- section3/main.go
+++ section3/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 )
+
+type Words struct {
+	Page  string   `json:"page"`
+	Input string   `json:"input"`
+	Words []string `json:"words"`
+}
 
 func main() {
 	args := os.Args
@@ -38,6 +46,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("HTTP Status Code: %d\n", res.StatusCode)
-	fmt.Printf("Body: %s\n", body)
+	if res.StatusCode != 200 {
+		fmt.Errorf("Invalid output(HTTP code %d): %s\n", res.StatusCode, body)
+		os.Exit(1)
+	}
+
+	var words Words
+
+	err = json.Unmarshal(body, &words)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("JSON Parsed:\n")
+	fmt.Printf("- page: %s\n", words.Page)
+	fmt.Printf("- input: %v\n", words.Input)
+	fmt.Printf("- words: %s\n", strings.Join(words.Words, ", "))
 }
