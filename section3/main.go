945b1f674b15042199d1ec264691267dfa9c69fd
cpmachado <cpmachado@protonmail.com>
2024-12-22 22:43:08 +0000

    Section 3 - Chapter 6
    Hello World with command arguments

--- section3/main.go
+++ section3/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	args := os.Args
 	fmt.Println("hello, world")
+	fmt.Printf("Arguments: %v\n", args)
+	fmt.Printf("Arguments without program: %v\n", args[1:])
 }
